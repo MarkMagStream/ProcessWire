b20df59bcf7844b313cd87bf93d5c32383d086a3
Mark van Eijk (MagStream) <mark@magstream.com>
2015-03-09 10:17:19 +0100

    Removed default install files.

--- site-beginner/templates/home.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-// The homepage uses same layout as basic-page template,
-// so we'll just include that as our output for homepage.
-// See basic-page.php for more.
-
-include("./basic-page.php"); 
